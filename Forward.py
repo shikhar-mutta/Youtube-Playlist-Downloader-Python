f9619d2f3a86bad0f104c1ade09919594e02a363
shikhar-mutta <78760296+shikhar-mutta@users.noreply.github.com>
2022-11-05 02:39:49 +0530

    Update Forward.py

--- Forward.py
+++ Forward.py
@@ -4,7 +4,7 @@ from pytube.cli import on_progress
 
 
 playlist = Playlist(input('Enter URL: \n')).video_urls
-count = 0
+count =0
 for video_link in playlist:
     count=count+1
     if count > 0: 
@@ -15,3 +15,11 @@ for video_link in playlist:
         except:
             print(str(count) + " unavailable"+ YouTube(video_link).get_highest_resolution())
 
+
+
+# # list = ["https://www.youtube.com/watch?v=x0OLat0fkzQ","https://www.youtube.com/watch?v=8l4ZjN4MXuQ","https://www.youtube.com/watch?v=C12WRs6LiQs","https://www.youtube.com/watch?v=rt6sHVfzjWM","https://www.youtube.com/watch?v=9Q6gFxqt9SU", "https://www.youtube.com/watch?v=SbSTTfnEMZM&t=899s" , "https://www.youtube.com/watch?v=B7cxLO1qKSw","https://www.youtube.com/watch?v=C2_L0wpY0iQ", "https://www.youtube.com/watch?v=OEP-Q3wVIpI" ]
+# # count = 0
+# # for i in list:
+# #     count= count+1
+# #     sm = YouTube(i , on_progress_callback=on_progress).streams.filter(res="720p").first().download(output_path="D:\Movie/30th oct", filename_prefix=str(count) +" " )
+# #     print(count)
